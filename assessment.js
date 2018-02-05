76268481678633130b34c9ca640bd08efbcab67c
niuniu24 <36141325+niuniu24@users.noreply.github.com>
2018-02-05 11:21:58 +0900

    ツイートボタン表示の修正

--- assessment.js
+++ assessment.js
@@ -39,7 +39,7 @@
         + encodeURIComponent(result);
         anchor.setAttribute('href', hrefValue);
         anchor.className = 'twitter-hashtag-button';
-        anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
+        anchor.innerText = 'Tweet #あなたのいいところ をツイートする';
         tweetDivided.appendChild(anchor);
 
         twttr.widgets.load();
